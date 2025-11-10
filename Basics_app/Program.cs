cfa5c1ba32996d883e189f6c47932a22f1b0a345
Geyasri-Chavala <geyasri.fullstack@gmail.com>
2025-11-10 16:21:37 +0530

    Add project files.

---
+++ Basics_app/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+
+
+class MyProgram
+    {
+        static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+        
+        // Numerical Data Types numbers
+        // byte, short, int, uint, long, ulong, decimal and double
+
+        byte age = 23;                                  // byte range from 0-255
+        Console.WriteLine(age);
+
+
+        short value = 1212;                            // Range from 32767 to -32768
+        short value1 = -12;
+        Console.WriteLine(value);
+        Console.WriteLine(value1);
+
+
+        ushort population = 50000;                   // Range from 0 to 65535   -- stores small number values
+        Console.WriteLine(population);
+
+
+        // int -- default data types for integers
+        // Range from -2,147,483,648 to 2,147,483,647
+
+        int salary = 25000;
+        Console.WriteLine(salary);  //integer 
+
+
+        //uint range from  -0 to 4,294,967,295
+          uint distance = 30000000U;
+
+        Console.WriteLine(distance);
+        Console.WriteLine(100);  //int
+        Console.WriteLine(100U); //Uint
+
+
+        //Long
+        // Range from -9223372036854775888 to 9223372036854775807
+        long worldpopulation = 7800000000L;
+        Console.WriteLine(worldpopulation);
+
+
+        //ulong 
+        // Range from 0 to -10,000,000,00,000,000
+        ulong indiaGDP = 20000000000UL;
+        Console.WriteLine(indiaGDP);
+
+
+
+
+        // Floating Data Types
+        // 0.68,    0.1215,     10,     10.0
+
+        // decimal 
+        // Range from 1.0*10^-28 to 7.9*10^28
+        // purpose mainly used for money or financial transactions
+        decimal price = 199.99M;
+        Console.WriteLine(price);
+
+        // float
+        // Range from -3.40282347*10^38 to 3.40282347*10^38
+        // purpose used in scientific  calculations.
+        float pi = 3.14f;
+        Console.WriteLine(pi);
+
+        // Double
+        // Range from +- 5.0810^-324 * +- 1.7*10^388
+        // Purpose Mathematical Calculations(stocks)
+
+        double db = 987.898d;
+        Console.WriteLine(db);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Basics_app/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Basics_app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basics_app")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bce6156-250b-4311-a81f-be007520f738")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
