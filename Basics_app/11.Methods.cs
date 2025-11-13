47d17c5c527fa03ce1692a142a154bc7661d01d5
Geyasri-Chavala <geyasri.fullstack@gmail.com>
2025-11-13 22:38:20 +0530

    methodNonReturn

---
+++ Basics_app/11.Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Methods
+{
+    static void Main()
+    {
+        Greet();
+
+        DisplayMessage();
+
+        ShowDetails();
+    }
+    // Void - Non-return type
+    static void Greet()
+        {
+            Console.WriteLine("Good Morning!");
+        }
+
+    // Void - Non-return type
+    static void DisplayMessage()
+       {
+            Console.WriteLine("Welcome to the C# world!");
+       }
+
+    static void ShowDetails()
+    {
+        Console.WriteLine("Name: Geya");
+
+        Console.WriteLine("fullNmae: Geya Sri chavala");
+    }
+
+}
+
