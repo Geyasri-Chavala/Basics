f1672c08f1153cdef4bbf5967c5d4f677d927a80
Geyasri-Chavala <geyasri.fullstack@gmail.com>
2025-11-13 19:28:18 +0530

    LoopingMech

---
+++ Basics_app/LoopingMechanisms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class LoopingMecanisams
+{
+    static void Main()
+    {
+        // while loop
+
+        int i = 1;
+
+        while (i <= 5)
+        {
+            Console.WriteLine("Number: " + i);
+            i++;
+        }
+
+        // Do While Loop
+
+        int result = 0;
+        do
+        {
+            Console.WriteLine("Do....Excuted");
+            result = result + 1;
+        } while (result < 5);
+
+        // For each Loop
+
+        string[] fruits = { "Apple", "Banana", "Cherry" };
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine("Fruit: " + fruit);
+        }
+
+
+    }
+}
